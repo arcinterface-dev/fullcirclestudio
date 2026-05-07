1ef0ae619951bc37010057ba97d44284dbc9a9a2
santhanakrishnanstark <santhanakrishnanstark@gmail.com>
2026-05-07 07:43:38 +0530

    chore: revert cloudflare configurations and switch to static export

--- open-next.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig({
-    // This helper sets up the "wrapper" and "converter" for you 
-    // so you don't have to write that long object manually.
-});
